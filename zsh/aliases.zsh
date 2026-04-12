cbb44f9561d598ddf7042bf201ab8b7881b08384
mengtao li <2741847511@qq.com>
2026-04-12 20:24:07 +0800

    update git related aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,5 +9,20 @@ else
   alias lla='ls -alh'
 fi
 
+# system
 alias ..='cd ..'
 alias ...='cd ../..'
+
+# git
+alias ginit='git init'
+alias gs='git status'
+alias gss='git status -s'
+alias ga='git add'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gb='git branch'
+alias gco='git checkout'
+alias glog='git log'
+alias glast='git log -1'
+alias ggraph='git log --oneline --graph --decorate --all'
+alias gunstage='git restore --staged --'
